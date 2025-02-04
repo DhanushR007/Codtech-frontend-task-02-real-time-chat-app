d185540e3a4dc7df8522722775f289683f0c15f5
DhanushR007 <dhanushmahi502@gmail.com>
2025-02-04 20:50:56 +0530

    Add files via upload

---
+++ src/components/ChatMessage.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function ChatMessage({ text, name, logo, user, email }) {
+  return (
+    <div>
+      {user.email === email ? (
+        <div className="d-flex justify-content-end right-content">
+          <div className="message-name"> {name}</div>
+          <span className="message-right">
+            <span className="message-text">{text}</span>
+            <img src={logo} alt="logo" className="logo-icon" />
+          </span>
+        </div>
+      ) : (
+        <div className="d-flex justify-content-end left-content">
+          <div className="message-name">{name}</div>
+          <span className="message-left">
+            <img src={logo} alt="logo" className="logo-icon" />
+            <span className="message-text">{text}</span>
+          </span>
+        </div>
+      )}
+    </div>
+  );
+}
